fb18172726ebb74893585e83c9a131a111c212dd
FreshleBlanc <dchang4309@gmail.com>
2022-11-15 09:32:31 -0600

    first commit

---
+++ day1-hw.py
@@ -0,0 +1,21 @@
+class Game:
+    def __init__(self, player1, computer):
+        self.player1 = player1
+        self.computer = computer
+
+
+
+
+class Player:
+    def __init__(self)
+    
+
+    def choice(self):
+       self.choice = input('Rock, Paper, Scissors?')
+
+
+
+
+class Computer:
+    def __init__(self)
+    
